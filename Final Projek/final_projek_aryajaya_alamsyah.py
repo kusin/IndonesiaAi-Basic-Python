8a9655ccbbf19c3beb9a3114fa90020644c6fc6e
kusin <aryajayaalamsyah@gmail.com>
2021-11-07 05:37:37 +0700

    -

---
+++ Final Projek/final_projek_aryajaya_alamsyah.py
@@ -0,0 +1,17 @@
+# PROGRAM Pendalaman-Perulangan
+
+
+# DEKLARASI Pustaka
+import builtins
+import os # os, pustaka mengenali os yg digunakan
+import smtplib
+import ssl
+from smtplib import SMTP
+from smtplib import SMTPException
+
+    
+# ALGORITMA
+if __name__=="__main__":
+    # Hapus layar / clear screen
+    # Baris pertama dari keluaran program adalah masukan program bukan path directory program. 
+    os.system('cls' if os.name == 'nt' else 'clear')
